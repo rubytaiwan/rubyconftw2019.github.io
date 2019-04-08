06d6db26f87e96a8aaee7aee73dda4e6ebce286e
Irene Chang <irene84111@gmail.com>
2019-04-08 18:43:56 +0800

    updated head and favicons

--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -6,7 +6,7 @@ module ViewHelpers
 
   def inline_svg(name)
     root = Middleman::Application.root
-    file_path = "#{root}/source/assets/images/#{name}.svg"
+    file_path = "#{root}/source/images/mixins/#{name}.svg"
     return File.read(file_path) if File.exists? (file_path)
     "(not found)"
   end
