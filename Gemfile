69ecb5792448fac6c36e6a8ab71ba8a8ae30f827
Irene Chang <irene84111@gmail.com>
2019-04-11 12:11:41 +0800

    added modal for coc and team-members

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'middleman-livereload', '~> 3.4'
 gem 'middleman-sprockets', '~> 4.1'
 gem 'middleman-deploy', '~> 2.0.0.pre.alpha'
 
+gem 'redcarpet'
+
 # Template
 gem 'slim'
 
--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -11,4 +11,26 @@ module ViewHelpers
     "(not found)"
   end
 
+  def markdown(text)
+    options = {
+      link_attributes: { target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true,
+      prettify: true
+    }
+
+    extensions = {
+      autolink: true,
+      superscript: true,
+      highlight: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
+
 end
